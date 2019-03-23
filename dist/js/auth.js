a86420c7f6bdd95f6ecfd97d78afc19697fe4865
Lufter <lufter21@gmail.com>
2019-03-23 23:41:17 +0200

    c

---
+++ dist/js/auth.js
@@ -0,0 +1,41 @@
+function socAuth(data) {
+	if(data){
+		$.ajax({
+			url:"/functions/auth.php",
+			type:"POST",
+			dataType:"json",
+			data: 'token='+ data,
+			success: function(response){
+				$('#js_auth_msg').empty().append('<span>'+ response.msg +'</span>');
+				if (response.msg == 'login') {
+					window.location.reload();
+				}
+			},
+			error: function() {
+				alert('js:Error');
+			} 
+		});
+	}
+}
+
+
+$(document).ready(function(){
+	Form.submit('#js-login-form, #js-registration-form', function(form) {
+		var _f = $(form);
+		$.ajax({
+			url: _f.attr('action'),
+			type:"POST",
+			dataType:"json",
+			data: _f.serialize(),
+			success: function(response){
+				$('#js_auth_msg').empty().append('<span>'+ response.msg +'</span>');
+				if (response.msg == 'login') {
+					window.location.reload();
+				}
+			},
+			error: function() {
+				alert('js:Error');
+			}
+		});
+	});
+});
---
+++ dist/js/common1.js
@@ -0,0 +1,48 @@
+function scriptLoaded() {
+	
+	var winW = $(window).width(),
+	winH = $(window).height();
+
+	$('.wrapper').css('padding-bottom', $('.footer').innerHeight());
+	
+	//showBlock
+	$('body').on('click', '.js-show-button', function() {
+		var _ = $(this),
+		_block = $('#'+ _.attr('data-block'));
+		if (!_.hasClass('active')) {
+			_block.slideDown(321);
+			_.addClass('active');
+		} else {
+			_block.slideUp(321);
+			_.removeClass('active');
+		}
+		return false;
+	});
+	
+	//header
+	$(window).scroll(function() {
+		if ($(window).scrollTop() > 7) {
+			$('.header').addClass('header_fixed');
+		} else {
+			$('.header').removeClass('header_fixed');
+		}
+	});
+	
+	if (adBlock) {
+		$('#alert').fadeIn(321).html('В вашем браузере установленно расширение Adblock, оно может помешать работе сервиса и вам не насчитают кэшбэк.');
+		$('.wrapper').css('padding-top', $('.header').innerHeight());
+	}
+	
+	
+	
+	if (selRegion) {
+		setTimeout(function(){
+			$('#menu-region').addClass('bg-red').slideDown(321);
+			$('#show-region-button').addClass('active');
+			if(winW < 1000){
+				$('.header__user-btn-mob').click();
+			}
+		},1021);
+	}
+	
+}
