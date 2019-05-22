527267bf41044a27da599bda2bf7bc5c7de7abb0
Lufter <lufter21@gmail.com>
2019-05-22 12:41:24 +0300

    com

--- dist/templates/inc/footer.php
+++ dist/templates/inc/footer.php
@@ -44,25 +44,19 @@
 <!--/POPUP-->
 
 <!-- Yandex.Metrika counter -->
-<script type="text/javascript">
-	(function(m, e, t, r, i, k, a) {
-		m[i] = m[i] || function() {
-			(m[i].a = m[i].a || []).push(arguments)
-		};
-		m[i].l = 1 * new Date();
-		k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
-	})
-	(window, document, "script", "https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js", "ym");
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js", "ym");
 
-	ym(39630900, "init", {
-		clickmap: true,
-		trackLinks: true,
-		accurateTrackBounce: true
-	});
+   ym(39630900, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
 </script>
-<noscript>
-	<div><img src="https://mc.yandex.ru/watch/39630900" style="position:absolute; left:-9999px;" alt="" /></div>
-</noscript>
+<noscript><div><img src="https://mc.yandex.ru/watch/39630900" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <!-- /Yandex.Metrika counter -->
 
 <!-- Global site tag (gtag.js) - Google Analytics -->
